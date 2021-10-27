62b07fe450da6fcb2f6cdbc5286bdfffee713a4a
Kaoru Chisen <cordx56@cordx.net>
2021-10-27 22:02:30 +0900

    modify cxsh ascii art

--- src/messages.rs
+++ src/messages.rs
@@ -1,12 +1,14 @@
 pub fn welcome_message() {
-    println!(r"                      _");
-    println!(r"                     | |");
-    println!(r"  ___   __  __  ___  | |__");
-    println!(r" /  __\ \ \/ / / __\ |  _ \");
-    println!(r"|  (__   >  <  \__ \ | | | |");
-    println!(r" \____/ /_/\_\ \___/ |_| |_|");
+    println!(r"   __________    ______________     ___");
+    println!(r"  /      \   \  /   /      \   |   |   |");
+    println!(r" /    ____\   \/   /   _____\  |___|   |");
+    println!(r"|    /     \      /        \           |");
+    println!(r"|    \____ /      \_____    \   ___    |");
+    println!(r" \        /   /\   \        /  |   |   |");
+    println!(r"  \______/___/  \___\______/___|   |___|");
+    println!(r"");
     println!(r"Welcome to cxsh!");
     println!(r"Type 'help' for instructions on how to use cxsh.");
-    println!(r"source code/bug report: https://github.com/cordx56/cxsh");
+    println!(r"source code / bug report: https://github.com/cordx56/cxsh");
     println!(r"");
 }
