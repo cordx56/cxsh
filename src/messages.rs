0c00b8282510a2375aae700aede02eca39cd7501
Kaoru Chisen <cordx56@cordx.net>
2021-10-27 22:08:26 +0900

    add help function

--- src/messages.rs
+++ src/messages.rs
@@ -9,6 +9,10 @@ pub fn welcome_message() {
     println!(r"");
     println!(r"Welcome to cxsh!");
     println!(r"Type 'help' for instructions on how to use cxsh.");
-    println!(r"source code / bug report: https://github.com/cordx56/cxsh");
+    println!(r"source code/bug report: https://github.com/cordx56/cxsh");
     println!(r"");
+}
+
+pub fn help() {
+    println!(r"available builtin commands: cd, help, exit");
 }
