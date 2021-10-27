4bb0db035d54673738567a51513d29623d75daf3
Kaoru Chisen <cordx56@cordx.net>
2021-10-27 18:22:01 +0900

    support multi-width char input

--- src/termio.rs
+++ src/termio.rs
@@ -5,6 +5,7 @@ use termion::cursor;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use unicode_width::{UnicodeWidthChar, UnicodeWidthStr};
 
 fn string_slice(s: &str, start: usize, end: usize) -> String {
     let mut result = String::new();
@@ -33,73 +34,76 @@ pub fn read() -> String {
 
     let mut buffer = String::new();
     let mut cursor_pos: usize = 0;
-    for key in stdin.keys() {
-        match key {
-            Ok(Key::Char(c)) => {
-                if c == '\n' {
+    for key_result in stdin.keys() {
+        match key_result {
+            Ok(key) => match key {
+                Key::Char('\n') => {
                     write!(stdout, "\r\n");
                     stdout.flush().ok();
                     return buffer;
-                } else if c == '\t' {
-                } else {
+                }
+                Key::Char('\t') => {}
+                Key::Char(c) => {
                     clear_command(&mut stdout, &buffer, cursor_pos);
                     let cursor_left = string_slice(&buffer, 0, cursor_pos);
                     let cursor_right = string_slice(&buffer, cursor_pos, buffer.len());
                     buffer = format!("{}{}{}", cursor_left, c, cursor_right);
                     write!(stdout, "{}", buffer);
                     cursor_pos += 1;
-                    let move_left = (buffer.len() - cursor_pos) as u16;
+                    let move_left = UnicodeWidthStr::width(&cursor_right as &str);
                     cursor_to_left(&mut stdout, move_left);
                     stdout.flush().ok();
                 }
-            }
-            Ok(Key::Ctrl(c)) => {
-                if c == 'c' {
-                    write!(stdout, "\r\n");
-                    stdout.flush().ok();
-                    return String::new();
+                Key::Ctrl(c) => {
+                    if c == 'c' {
+                        write!(stdout, "\r\n");
+                        stdout.flush().ok();
+                        return String::new();
+                    }
                 }
-            }
-            Ok(Key::Backspace) => {
-                if 0 < cursor_pos {
-                    clear_command(&mut stdout, &buffer, cursor_pos);
-                    let cursor_left = string_slice(&buffer, 0, cursor_pos - 1);
-                    let cursor_right = string_slice(&buffer, cursor_pos, buffer.len());
-                    buffer = format!("{}{}", cursor_left, cursor_right);
-                    write!(stdout, "{}", buffer);
-                    cursor_pos -= 1;
-                    let move_left = (buffer.len() - cursor_pos) as u16;
-                    cursor_to_left(&mut stdout, move_left);
-                    stdout.flush().ok();
+                Key::Backspace => {
+                    if 0 < cursor_pos {
+                        clear_command(&mut stdout, &buffer, cursor_pos);
+                        let cursor_left = string_slice(&buffer, 0, cursor_pos - 1);
+                        let cursor_right = string_slice(&buffer, cursor_pos, buffer.len());
+                        buffer = format!("{}{}", cursor_left, cursor_right);
+                        write!(stdout, "{}", buffer);
+                        cursor_pos -= 1;
+                        let move_left = UnicodeWidthStr::width(&cursor_right as &str);
+                        cursor_to_left(&mut stdout, move_left);
+                        stdout.flush().ok();
+                    }
                 }
-            }
-            Ok(Key::Delete) => {
-                if cursor_pos < buffer.len() {
-                    clear_command(&mut stdout, &buffer, cursor_pos);
-                    let cursor_left = string_slice(&buffer, 0, cursor_pos);
-                    let cursor_right = string_slice(&buffer, cursor_pos + 1, buffer.len());
-                    buffer = format!("{}{}", cursor_left, cursor_right);
-                    write!(stdout, "{}", buffer);
-                    let move_left = (buffer.len() - cursor_pos) as u16;
-                    cursor_to_left(&mut stdout, move_left);
-                    stdout.flush().ok();
+                Key::Delete => {
+                    if cursor_pos < buffer.len() {
+                        clear_command(&mut stdout, &buffer, cursor_pos);
+                        let cursor_left = string_slice(&buffer, 0, cursor_pos);
+                        let cursor_right = string_slice(&buffer, cursor_pos + 1, buffer.len());
+                        buffer = format!("{}{}", cursor_left, cursor_right);
+                        write!(stdout, "{}", buffer);
+                        let move_left = UnicodeWidthStr::width(&cursor_right as &str);
+                        cursor_to_left(&mut stdout, move_left);
+                        stdout.flush().ok();
+                    }
                 }
-            }
-            Ok(Key::Left) => {
-                if 0 < cursor_pos {
-                    cursor_pos -= 1;
-                    write!(stdout, "{}", cursor::Left(1));
-                    stdout.flush().ok();
+                Key::Left => {
+                    if 0 < cursor_pos {
+                        cursor_pos -= 1;
+                        let move_left = UnicodeWidthChar::width(buffer.chars().nth(cursor_pos).unwrap()).unwrap();
+                        write!(stdout, "{}", cursor::Left(move_left as u16));
+                        stdout.flush().ok();
+                    }
                 }
-            }
-            Ok(Key::Right) => {
-                if cursor_pos < buffer.len() {
-                    cursor_pos += 1;
-                    write!(stdout, "{}", cursor::Right(1));
-                    stdout.flush().ok();
+                Key::Right => {
+                    if cursor_pos < buffer.len() {
+                        let move_right = UnicodeWidthChar::width(buffer.chars().nth(cursor_pos).unwrap()).unwrap();
+                        cursor_pos += 1;
+                        write!(stdout, "{}", cursor::Right(move_right as u16));
+                        stdout.flush().ok();
+                    }
                 }
-            }
-            Ok(_) => {}
+                _ => {}
+            },
             Err(_) => {
                 write!(stderr, "stdin read error\r\n");
             }
@@ -110,18 +114,19 @@ pub fn read() -> String {
 
 fn clear_command(stdout: &mut dyn Write, buffer: &str, cursor_pos: usize) {
     if 0 < cursor_pos {
-        write!(stdout, "{}", cursor::Left(cursor_pos as u16));
+        let cursor_left = string_slice(&buffer, 0, cursor_pos);
+        write!(stdout, "{}", cursor::Left(UnicodeWidthStr::width(&cursor_left as &str) as u16));
     }
-    for _ in 0..buffer.len() {
+    for _ in 0..UnicodeWidthStr::width(buffer) {
         write!(stdout, " ");
     }
     if 0 < buffer.len() {
-        write!(stdout, "{}", cursor::Left(buffer.len() as u16));
+        write!(stdout, "{}", cursor::Left(UnicodeWidthStr::width(buffer) as u16));
     }
 }
 
-fn cursor_to_left(stdout: &mut dyn Write, n: u16) {
+fn cursor_to_left(stdout: &mut dyn Write, n: usize) {
     if 0 < n {
-        write!(stdout, "{}", cursor::Left(n));
+        write!(stdout, "{}", cursor::Left(n as u16));
     }
 }
